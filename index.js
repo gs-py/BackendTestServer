7d961064396b702cf2722c995711e5757dc78431
Gladwin <75156799+xakep-8@users.noreply.github.com>
2024-12-12 11:17:11 +0530

    Cahnged Cors

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
+
 const { faker } = require("@faker-js/faker");
 
 app.listen(process.env.PORT, () => {
